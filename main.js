3fc083c0621115fd1167942cd1acabbf79c9269b
Danilo <danilonogueiraleite@gmail.com>
2023-02-07 19:02:34 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form"); {
+    return num2 > num1;}
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const teste = document.getElementById(num1);
+    const teste = document.getElementById(num2);
+    if (parseInt(num2) > parseInt(num1)) {
+        alert("Segundo numero e maior que o primeiro");
+    } else
+    alert("Campo invalido")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* const form = document.getElementById('form')
+
+function validnumeros(num1, num2) {
+    return
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
+
+function valid (num1, num2) {
+    let result;
+if (num2 > num1) {
+    alert("Seu formulário está correto");
+} else {
+    alert("Seu formulário está incorreto");
+}}
+
+
+
+let num1 = parseFloat(document.getElementById('numero1'));
+let num2 = parseFloat(document.getElementById('numero2')); */
