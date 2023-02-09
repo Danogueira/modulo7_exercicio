22715a50df918b2c55db26943e4d7a98220fe2ed
Danogueira <122055174+Danogueira@users.noreply.github.com>
2023-02-09 07:49:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
-const form = document.querySelector("form"); {
-    return num2 > num1;}
+function maiorq (num1, num2) {
+const form = document.querySelector("form"); 
+    return num2 > num1;
+}
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
     const teste = document.getElementById(num1);
-    const teste = document.getElementById(num2);
-    if (parseInt(num2) > parseInt(num1)) {
+    const teste2 = document.getElementById(num2);
+    if teste2 > teste {
         alert("Segundo numero e maior que o primeiro");
     } else
     alert("Campo invalido")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-/* const form = document.getElementById('form')
-
-function validnumeros(num1, num2) {
-    return
-}
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-})
-
-function valid (num1, num2) {
-    let result;
-if (num2 > num1) {
-    alert("Seu formulário está correto");
-} else {
-    alert("Seu formulário está incorreto");
-}}
-
-
-
-let num1 = parseFloat(document.getElementById('numero1'));
-let num2 = parseFloat(document.getElementById('numero2')); */
